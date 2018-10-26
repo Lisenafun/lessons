8920a3a866834a6327b9bf3ca4173ef6aeb1bfa2
OlesiaViazankina <lisena.cmex@rambler.ru>
2018-10-26 21:48:04 +0300

    урок 5 конспект полностью

---
+++ src/lesson5/Main.java
@@ -0,0 +1,10 @@
+package lesson5;
+
+import lesson5.battleunit.BattleUnit;
+
+public class Main {
+    public static void main(String[] args) {
+        BattleUnit battleUnit = new BattleUnit(90,67);
+
+    }
+}
